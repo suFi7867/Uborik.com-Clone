347181f03afbe694bfc480bba16a0678587d15ef
Prashant Anand <906120anand@gmail.com>
2022-07-22 12:48:10 +0530

    kk

--- Product-DAY2/data.js
+++ Product-DAY2/data.js
@@ -1,44 +1,44 @@
-var data = {
-  0: {
+var data = [
+  {
     company_name: "CLASSIC BLACK",
     tittle: "THE GENTLEMAN EDITION SOCKS",
-    price: "399.00",
+    price: "399",
     description:
       "Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
     src: "https://www.uboric.com/wp-content/uploads/2020/11/Gentleman_600x.png",
   },
-  1: {
+  {
     company_name: "Luxury Terry",
     tittle: "ASHIKAGA EDITION SOCKS",
-    price: "299.00",
+    price: "299",
     description: "Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks",
     src: "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-fun-men-socks-stripes-ashikaga-edition-13861452611635_600x.png",
   },
-  2: {
+  {
     company_name: "CLASSIC BLACK",
     tittle: "CLASSIC BLACK EDITION SOCKS",
-    price: "349.00",
+    price: "349",
     description:
       "Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
     src: "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-men-socks-black-classic-black-edition-13861452644403_600x.png",
   },
-  3: {
+  {
     company_name: "CLASSIC BLACK",
     tittle: "HAPPY SOCKS GIFT BOX FOR MEN",
-    price: "1,599.00",
+    price: "1599",
     description:
       "Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
     src: "https://www.uboric.com/wp-content/uploads/2020/11/POWERBOX_c4c4b190-aba2-4629-8bfe-8e7d0507d99b_600x.png",
   },
-  4: {
+  {
     company_name: "QOR9",
     tittle: "TRUETUNE in-Ear True Wireless Earphones",
-    price: "1,399.00",
+    price: "1399.00",
     description:
       "QOR9 TRUETUNE in-Ear True Wireless Earphones 5.0 Bluetooth Headphones, Touch Control Ear Pods, Earphones (BLACK)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51hsqPN9mL._SL1080_.jpg",
   },
-  5: {
+  {
     company_name: "QOR9",
     tittle: "TRUEBASS Wireless Earbuds Bluetooth",
     price: "1,599.00",
@@ -46,7 +46,7 @@ var data = {
       "QOR9 TRUEBASS Wireless Earbuds Bluetooth 5.0 Headphones Noise Cancelling Mini Charging Case IPX5 Waterproof in-Ear Earbuds Built-in Mic Stereo Sound with Deep Bass for iOS/Android/Windows Earbuds (Black)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51L5yIwOFcL._SL1080_.jpg",
   },
-  6: {
+  {
     company_name: "QOR9",
     tittle: "TRUEBASS Wireless Earbuds Bluetooth 5.0",
     price: "1,549.00",
@@ -54,31 +54,31 @@ var data = {
       "QOR9 TRUEBASS Wireless Earbuds Bluetooth 5.0 Headphones Noise Cancelling Mini Charging Case IPX5 Waterproof in-Ear Earbuds Built-in Mic Stereo Sound with Deep Bass for iOS/Android/Windows Earbuds (Black)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51L5yIwOFcL._SL1080_.jpg",
   },
-  7: {
+  {
     company_name: "QOR9",
     tittle: "TRUETUNE in-Ear True Wireless Earphones",
-    price: "1,399.00",
+    price: "1399.00",
     description:
       "QOR9 TRUETUNE in-Ear True Wireless Earphones 5.0 Bluetooth Headphones, Touch Control Ear Pods, Earphones (BLACK)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51hsqPN9mL._SL1080_.jpg",
   },
-  8: {
+  {
     company_name: "Aroma7",
     tittle: "CASUL CHAPPAL/SLIPPERS",
-    price: 79,
+    price: "79",
     description:
       "Aroma Double Soap Dish Anti Rust, Corrosion-Free 304 Stainless Steel Double Soap Dish with Glass and Silver Finish, Twin soap dish",
     src: "https://www.uboric.com/wp-content/uploads/2019/12/fe4618c043ad5d860331149ed80888ef_result.jpg",
   },
-  9: {
+  {
     company_name: "QOR9",
     tittle: "TRUETUNE in-Ear True Wireless Earphones",
-    price: "1,399.00",
+    price: "1399.00",
     description:
       "QOR9 TRUETUNE in-Ear True Wireless Earphones 5.0 Bluetooth Headphones, Touch Control Ear Pods, Earphones (BLUE)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51I6oH4f4L._SL1080_.jpg",
   },
-  10: {
+  {
     company_name: "QOR9",
     tittle: "TRUEBASS Wireless Earbuds Bluetooth 5.0",
     price: "1,549.00",
@@ -86,15 +86,15 @@ var data = {
       "QOR9 TRUEBASS Wireless Earbuds Bluetooth 5.0 Headphones Noise Cancelling Mini Charging Case IPX5 Waterproof in-Ear Earbuds Built-in Mic Stereo Sound with Deep Bass for iOS/Android/Windows Earbuds (Black)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51L5yIwOFcL._SL1080_.jpg",
   },
-  11: {
+  {
     company_name: "Aroma",
     tittle: "CROCS",
-    price: 17,
+    price: "17",
     description:
       "Aroma Stainless Steel 304 Grade 2 in 1 Soap Dish with Soap Dispenser/Bathroom Accessories",
     src: "https://www.uboric.com/wp-content/uploads/2019/12/0948bcac0931b8910d218f75d40c745f_result-400x400.jpg",
   },
-  12: {
+  {
     company_name: "QOR9",
     tittle: "TRUEBASS Wireless Earbuds Bluetooth",
     price: "1,599.00",
@@ -102,15 +102,15 @@ var data = {
       "QOR9 TRUEBASS Wireless Earbuds Bluetooth 5.0 Headphones Noise Cancelling Mini Charging Case IPX5 Waterproof in-Ear Earbuds Built-in Mic Stereo Sound with Deep Bass for iOS/Android/Windows Earbuds (Black)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51L5yIwOFcL._SL1080_.jpg",
   },
-  13: {
+  {
     company_name: "QOR9",
     tittle: "TRUETUNE in-Ear True Wireless Earphones",
-    price: "1,399.00",
+    price: "1399.00",
     description:
       "QOR9 TRUETUNE in-Ear True Wireless Earphones 5.0 Bluetooth Headphones, Touch Control Ear Pods, Earphones (BLACK)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/51hsqPN9mL._SL1080_.jpg",
   },
-  14: {
+  {
     company_name: "QOR9 CROWN",
     tittle: "BLACK Magnet Wireless Bluetooth Earphone",
     price: "799.00",
@@ -118,7 +118,7 @@ var data = {
       "QOR9 CROWN BLACK Magnet Wireless Bluetooth Earphone with Mic Magnetic Bluetooth Headphones with mic Stereo Headsets Hands-Free with Microphone and Neckband for Android and Apple Smartphones (Standard)",
     src: "https://www.uboric.com/wp-content/uploads/2021/02/1_result-e1615275145951.jpg",
   },
-  15: {
+  {
     company_name: "Aroma",
     tittle: "KIDS SPORTS SHOES",
     price: "799.00",
@@ -127,7 +127,7 @@ var data = {
       "Aroma Double Soap Dish Anti Rust, Corrosion-Free 304 Stainless Steel Double Soap Dish with Glass and Silver Finish, Twin soap dish",
     src: "https://www.uboric.com/wp-content/uploads/2019/12/20be3badaeebc381f66fa0ead8f4062a_result-400x400.jpg",
   },
-  16: {
+  {
     company_name: "Aroma",
     tittle: "big shoptool vegetable chopper (900ml) purple",
     price: "899.00",
@@ -136,7 +136,7 @@ var data = {
       "Smartly chop vegetables, salad, fruits and more also Big handle for easy pull, Non Skid Base",
     src: "https://www.uboric.com/wp-content/uploads/2020/12/61q57KwueBL._SL1280_-400x587.jpg",
   },
-  17: {
+  {
     company_name: "Aroma",
     tittle: "KIDS SPORTS SHOES",
     price: "899.00",
@@ -145,6 +145,6 @@ var data = {
       "Aroma Double Soap Dish Anti Rust, Corrosion-Free 304 Stainless Steel Double Soap Dish with Glass and Silver Finish, Twin soap dish",
     src: "https://www.uboric.com/wp-content/uploads/2019/12/20be3badaeebc381f66fa0ead8f4062a_result-400x400.jpg",
   },
-};
+];
 
 export default data;
--- Product-DAY2/productpage.js
+++ Product-DAY2/productpage.js
@@ -1,12 +1,97 @@
-import createProduct from "./createProduct.js";
-import productsData from "./data.js";
+import { header, footer } from "../../safe-table-3122/Components/common.js";
 
-const productContainer = document.querySelector(".products-cont");
+document.querySelector("#top_header").innerHTML = header();
+document.querySelector("#footer").innerHTML = footer();
+import data from "./data.js";
 
-console.log(productsData);
+let sort = document.getElementById("select_Sort");
+sort.addEventListener("change", sorting);
 
-const allDataStr = Object.keys(productsData)
-  .map((key) => createProduct(productsData[key],key))
-  .join("\n");
+function sorting() {
+  if (sort.value === "lth") {
+    let sort = data.sort((a, b) => {
+      if (Number(a.price) > Number(b.price)) return 1;
+      else if (Number(a.price) < Number(b.price)) return -1;
+      else return 0;
+    });
 
-productContainer.innerHTML = allDataStr;
+    Append(sort);
+  } else {
+    let sort = data.sort((a, b) => {
+      if (Number(a.price) > Number(b.price)) return -1;
+      else if (Number(a.price) < Number(b.price)) return 1;
+      else return 0;
+    });
+
+    Append(sort);
+  }
+}
+
+let mainDiv = document.getElementById("product_cont");
+
+Append(data);
+
+function Append(data) {
+  mainDiv.innerHTML = "";
+
+  data.forEach((el) => {
+    let mDiv = document.createElement("div");
+    mDiv.style.padding = "10px";
+    // mDiv.style.border = "1px solid red";
+
+    let iDiv = document.createElement("div");
+    iDiv.style.overflow = "flexbox";
+
+    let img = document.createElement("img");
+    img.setAttribute("src", el.src);
+    img.style.height = "150px";
+    img.style.width = "150px";
+    img.style.marginLeft = "15px";
+    iDiv.append(img);
+
+    let p = document.createElement("p");
+    p.innerText = el.tittle;
+    p.style.textAlign = "center";
+
+    let cutprice = document.createElement("div");
+    cutprice.style.display = "flex";
+    cutprice.style.marginTop = "20px";
+    cutprice.style.marginBottom = "20px";
+    cutprice.style.justifyContent = "center";
+    cutprice.style.alignItems = "center";
+    cutprice.innerText = "₹ " + el.price;
+
+    let cart = document.createElement("div");
+    cart.style.background = "black";
+    cart.style.color = "white";
+    cart.style.padding = "10px";
+    cart.style.display = "flex";
+    cart.style.justifyContent = "center";
+    cart.style.alignItems = "center";
+    cart.innerText = "Add To Cart";
+    cart.style.cursor = "pointer";
+    cart.addEventListener("click", function () {
+      addData(el);
+    });
+
+    mDiv.append(iDiv, p, cutprice, cart);
+
+    mainDiv.append(mDiv);
+  });
+}
+
+let arr = localStorage.getItem(JSON.stringify("carttt")) || [];
+
+function addData(el) {
+  let filtered = arr.filter((e) => {
+    return el.tittle == e.tittle;
+  });
+
+  if (filtered.length > 0) {
+    alert("Already Exists");
+  } else {
+    arr.push(el);
+    localStorage.setItem("carttt", JSON.stringify(arr));
+    alert("Product Added Successfully");
+  }
+}
